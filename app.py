2f5eb03cf9bf98fd393e92a9a8ac0e25191a5f5f
Furihata Hiroyuki <furihata@synclogue.com>
2014-11-12 15:08:14 +0900

    add business logic

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, url_for, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -25,7 +25,33 @@ class Todo(db.Model):
 
 @app.route('/')
 def show_all():
-    return render_template('index.html')
+    todos = Todo.query.order_by(Todo.pub_date.desc()).all()
+    return render_template('index.html', todos=todos)
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        if not request.form['title']:
+            flash('Title is required', 'error')
+        elif not request.form['text']:
+            flash('Title is required', 'error')
+        else:
+            todo = Todo(request.form['title'], request.form['text'])
+            db.session.add(todo)
+            db.session.commit()
+            flash('Todo item was successfully created')
+            return redirect(url_for('show_all'))
+    return render_template('new.html')
+
+
+@app.route('/update', methods=['POST'])
+def update_done():
+    for todo in Todo.query.all():
+        todo.done = ('done.%d' % todo.id) in request.form
+    flash('Updated status')
+    db.session.commit()
+    return redirect(url_for('show_all'))
 
 
 if __name__ == '__main__':
