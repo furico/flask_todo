ba7ae42a2e6ebe8919b20107af025eec70ef857c
Furihata Hiroyuki <furihata@synclogue.com>
2014-11-12 12:35:24 +0900

    init

---
+++ app.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column('todo_id', db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    text = db.Column(db.String)
+    done = db.Column(db.Boolean)
+    pub_date = db.Column(db.DateTime)
+
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+        self.done = False
+        self.pub_date = datetime.utcnow()
+
+
+@app.route('/')
+def show_all():
+    return "This is root!"
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ config.py
@@ -0,0 +1,4 @@
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/flask_todo_dev'
+    SECRET_KEY = 'this-is-secret-key'
+    DEBUG = True
